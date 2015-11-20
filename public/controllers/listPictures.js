3a8a35a50858fc9744adcf3acd99631d089118b9
p3t3revans <p3t3revans@live.com>
2015-11-20 13:27:28 +1100

    fixing the list

--- public/controllers/listPictures.js
+++ public/controllers/listPictures.js
@@ -5,7 +5,7 @@ angular.module('MyApp')
       'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
       'Y', 'Z'];
 
-    $scope.medium = ['Work on Paper', 'Work on Canvas', 'Drawing', 'Photograph', 'Clay',
+    $scope.mediums = ['Work on Paper', 'Work on Canvas', 'Drawing', 'Photograph', 'Clay',
       'Sculpture'];
 
     $scope.headingTitle = 'Top 12 Picture';
